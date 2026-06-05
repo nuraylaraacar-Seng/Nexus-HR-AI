d97f92d2fef47973cb91a92e8bb7c300b592ad88
Nuray Lara AÇAR <l45097721@gmail.com>
2026-06-05 18:09:54 +0300

    Update test_data_engine.py

--- Backend/test_data_engine.py
+++ Backend/test_data_engine.py
@@ -11,7 +11,7 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-#GELECEĞİN VERİ BİLİMCİSİ
+
 # --- TEST DATA/ TEST VERİSİ ---
 def make_sample_df():
     return pd.DataFrame({
