28c17b388eb20715c1cfa042d9d703331bd38433
Nuray Lara AÇAR <l45097721@gmail.com>
2026-06-05 18:04:19 +0300

    Update data_engine.py

--- Backend/data_engine.py
+++ Backend/data_engine.py
@@ -18,7 +18,7 @@ class HRDataEngine:
             (BASE_DIR / "Data").resolve(),
             (BASE_DIR / "Data" / "sessions").resolve(),
         ]
-        resolved = Path(file_path).resolve()
+        resolved = Path(file_path).
 
         #Path Traversal Attack prevention
         #Dizin Gezinme Saldırısı koruması
